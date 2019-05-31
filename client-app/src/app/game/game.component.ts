0dcf84b5830dbf4fd41643411e4b183e8b58f993
Przemek Smyrdek <przemek.smyrdek@gmail.com>
2019-05-31 19:45:23 +0200

    Start game on demand

--- client-app/src/app/game/game.component.ts
+++ client-app/src/app/game/game.component.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { GamesService } from '../_services/games.service';
-import { AppState } from '../_app-state/state';
-import { ActivatedRoute } from '@angular/router';
-import { Store } from '@ngrx/store';
-import { InitStateAction } from '../_app-state/actions/app.actions';
 
 @Component({
   selector: 'app-game',
@@ -13,24 +8,12 @@ import { InitStateAction } from '../_app-state/actions/app.actions';
 export class GameComponent implements OnInit {
 
   constructor(
-    private store: Store<AppState>,
-    private gamesService: GamesService,
-    private route: ActivatedRoute
+    
   ) { }
 
   ngOnInit() {
     
-    const gameId = this.route.snapshot.paramMap.get('gameId');
-
-    this.gamesService.joinGame(gameId)
-      .subscribe(
-        (state: AppState) => {
-          this.store.dispatch(new InitStateAction({state}))
-        },
-        (err) => {
-          alert(`Cannot join ${gameId}`)
-        }
-      )
+    
   }
 
 }
--- client-app/src/app/topbar/topbar.component.ts
+++ client-app/src/app/topbar/topbar.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../_app-state/state';
+import { GamesService } from '../_services/games.service';
+import { ActivatedRoute } from '@angular/router';
+import { InitStateAction } from '../_app-state/actions/app.actions';
 
 @Component({
   selector: 'app-topbar',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private store: Store<AppState>,
+    private gamesService: GamesService,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+  }
+
+  joinGame() {
+    const gameId = this.route.snapshot.paramMap.get('gameId');
+
+    this.gamesService.joinGame(gameId)
+      .subscribe(
+        (state: AppState) => {
+          this.store.dispatch(new InitStateAction({state}))
+        },
+        (err) => {
+          alert(`Cannot join ${gameId}`)
+        }
+      )
   }
 
 }
