b5e3c91d7647ab851691b8984bb54ed061545fb6
Przemek Smyrdek <przemek.smyrdek@gmail.com>
2019-06-05 17:26:11 +0200

    Update Angular, install material

--- client-app/src/app/_services/games.service.ts
+++ client-app/src/app/_services/games.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { GameMetadata } from '../_models/GameMetadata';
-import { AppState } from '../_app-state/state';
 
 @Injectable({ providedIn: 'root' })
 export class GamesService {
